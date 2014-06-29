f6c5eaab1a6b133424ebb546aad1a7268631c651
Guillaume Gentile <ewandor@dorfsvald.net>
2014-06-29 14:33:19 +0200

    Registering the home APP in the settings

--- dorfsvald_net/home/views.py
+++ dorfsvald_net/home/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Bienvenue sur dorfsvald.net")
+    template = loader.get_template('home/index.html')
+    context = RequestContext(request, {})
+    return HttpResponse(template.render(context))
